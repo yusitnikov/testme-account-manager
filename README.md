6aa95306ac59d0de3ebd69f0577d2b5984979d3c
yu.sitnikov@synergetica.co <yu.sitnikov@synergetica.co>
2015-12-09 20:53:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Kaltura TestMe Console Account Manager
+User Script for managing accounts in Kaltura TestMe Console and create sessions easily.
+
+## Release Notes
+
+### v1.0
+- Manage Kaltura accounts
+- Generate KS on account change
+- Show selected partner name under ks field
+
+
+## Installation
+- Install browser extension for running user scripts: GreaseMonkey for Firefox, TamperMonkey for Google Chrome and family
+- Install the script by going to the [following link|https://github.com/yusitnikov/testme-account-manager/blob/master/kaltura-testme-account-manager.user.js]
+
+## Usage
+- Go to Kaltura TestMe Console
+- Add your account details by pressing "+" button in the page header
+- Switch accounts by selecting them in the drop-down
+
+![Screenshot](https://i.gyazo.com/cb3da10bba79f52f77bd954d2654108e.png)
---
+++ kaltura-testme-account-manager.user.js
@@ -0,0 +1,103 @@
+// ==UserScript==
+// @name        Kaltura TestMe Console Account Manager
+// @description User Script for managing accounts in Kaltura TestMe Console and create sessions easily
+// @author      yusitnikov
+// @version     1.0
+// @updateURL   https://github.com/yusitnikov/testme-account-manager/blob/master/kaltura-testme-account-manager.user.js
+// @include     http://www.kaltura.com/api_v3/testme/*
+// @include     https://www.kaltura.com/api_v3/testme/*
+// @exclude     http://www.kaltura.com/api_v3/testme/client-libs.php
+// @exclude     https://www.kaltura.com/api_v3/testme/client-libs.php
+// @run-at      document-end
+// @require     https://code.jquery.com/jquery-2.1.4.min.js
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+	// prevent from loading twice
+	if ($('#ktksm-li').size()) return;
+	
+	var baseUrl = location.protocol + '//' + location.hostname + '/api_v3/';
+	
+	var map = GM_getValue('map') || {};
+	
+	function saveMap() {
+		GM_setValue('map', map);
+	}
+	
+	var $li = $('<li id="ktksm-li"><select></select></li>'), $select = $li.find('select'),
+		$li2 = $('<li><button>+</button></li>'), $add = $li2.find('button'),
+		$li3 = $('<li><button>-</button></li>'), $drop = $li3.find('button'),
+		$ksDetails = $('<div></div>'), $ks = $('#ks');
+	$('#kmcSubMenu').append($li).append($li2).append($li3);
+	$ks.parent().append($ksDetails);
+
+	var requestedPid;
+	$select.change(function() {
+		var pid = this.value;
+		if (!pid) return;
+		var info = map[pid], name = info.name, errMsg = 'Failed to create KS for ' + name;
+		$ks.val('');
+		$ksDetails.html('&nbsp;');
+		requestedPid = pid;
+		$.ajax({
+			url: baseUrl + '?service=session&action=start&secret=' + info.secret + '&type=2&partnerId=' + pid + '&format=1',
+			dataType: 'json',
+			success: function(ks) {
+				if (pid != requestedPid) return;
+				if (typeof ks != 'string') {
+					if (ks && ks.message) errMsg += ': ' + ks.message;
+					alert(errMsg);
+					return;
+				}
+				$ks.val(ks);
+				$.ajax({
+					url: baseUrl + '?service=partner&action=getInfo&format=1&ks=' + ks,
+					dataType: 'json',
+					success: function(partner) {
+						$ksDetails.html('PID ' + pid + ' - ' + partner.name);
+					},
+					error: function() {
+						$ksDetails.html('PID ' + pid + ' - ' + name);
+					}
+				});
+			},
+			error: function() {
+				alert(errMsg);
+			}
+		});
+	});
+
+	$add.click(function() {
+		var pid = prompt('Enter partnerId:');
+		if (!pid) return;
+		var name = prompt('Enter account name:');
+		if (!name) return;
+		var secret = prompt('Enter admin secret:');
+		if (!secret) return;
+		map[pid] = {
+			name: name,
+			secret: secret
+		};
+		saveMap();
+		addOption(pid, true);
+	});
+	
+	$drop.click(function() {
+		var pid = $select.val();
+		delete map[pid];
+		saveMap();
+		$select.find('[value=' + pid + ']').remove();
+		if (Object.keys(map).length) $select.change();
+	});
+
+	function addOption(pid, select) {
+		$select.append($('<option></option>').val(pid).text(map[pid].name + ' (#' + pid + ')'));
+		if (select) $select.val(pid).change();
+	}
+
+	for (var pid in map) addOption(pid);
+	if (Object.keys(map).length) $select.change();
+})();
